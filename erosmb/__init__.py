f34fda3876d22a974ec27b522475a434a1642310
Psyray <psyray@users.noreply.github.com>
2023-12-12 18:25:42 +0100

    Add file support, threads and progress

--- erosmb/__init__.py
+++ erosmb/__init__.py
@@ -2,6 +2,8 @@ import argparse
 import logging
 import ipaddress
 import threading
+import psutil
+from datetime import datetime
 
 from erosmb.Machine import Machine
 from erosmb.SMBScanner import SMBScanner
@@ -22,6 +24,7 @@ parser.add_argument("--username", default="anonymous")
 parser.add_argument("--password", default="anonymous", help="password for username")
 parser.add_argument("--domain", default="LANPARTY", help="domain for username")
 parser.add_argument("--nologo", default=False, action="store_true", help="do not display logo")
+parser.add_argument("--threads", default=255, help="set a number of threads (default: 255)")
 parser.add_argument("--nothreads", default=False, action="store_true", help="do not use multithreading")
 
 args = parser.parse_args()
@@ -31,7 +34,7 @@ if args.debug:
 elif args.verbose:
     logging.root.setLevel(logging.WARNING)
 else:
-    logging.root.setLevel(logging.ERROR)
+    logging.root.setLevel(logging.CRITICAL)
 
 log = logging.getLogger(__name__)
 formatter = logging.Formatter('%(levelname)s | %(name)s | %(message)s')
@@ -58,27 +61,71 @@ def common_scan(ip):
 
         machines.append(machine)
 
-
 def print_info(machine: Machine):
     try:
         answer = f"{Fore.GREEN}[{machine.ip:^15}]{Fore.RESET} " \
-                 f"{machine.os:<45} {Fore.YELLOW}{machine.arch}{Fore.RESET} " \
-                 f"[{Fore.CYAN}{machine.domain}\\\\{machine.name}{Fore.RESET}]"
+                f"{machine.os:<45} {Fore.YELLOW}{machine.arch}{Fore.RESET} " \
+                f"[{Fore.CYAN}{machine.domain}\\\\{machine.name}{Fore.RESET}]"
 
         if machine.logged_in:
             answer += f" {Fore.RED}Logged in as {args.username}{Fore.RESET}"
 
         if args.verbose or args.debug:
             print(answer,
-                  Fore.GREEN, "DNS:", machine.dns_name, "IsLoginReq:", machine.is_login_req,
-                  "SMBVer:", hex(machine.smb_dialect),
-                  Fore.RESET)
+                Fore.GREEN, "DNS:", machine.dns_name, "IsLoginReq:", machine.is_login_req,
+                "SMBVer:", hex(machine.smb_dialect),
+                Fore.RESET)
         else:
             print(answer)
     except AttributeError:
         answer = f"{Fore.RED}[{machine.ip:^15}]{Fore.RED} Error"
         print(answer)
 
+def is_valid_ip(ip):
+    try:
+        ipaddress.ip_address(ip)
+        return True
+    except ValueError:
+        return False
+
+def is_valid_ip_range(ip_range):
+    try:
+        ipaddress.ip_network(ip_range, strict=False)
+        return True
+    except ValueError:
+        return False
+
+def update_progress_bar(progress, total, target):
+    percentage = (progress / total) * 100
+    bar_length = 30
+    filled_length = int(bar_length * progress // total)
+    bar = '#' * filled_length + '-' * (bar_length - filled_length)
+    print(f"\rScanning {target}: |{Fore.GREEN}{bar}{Fore.RESET}| {percentage:.1f}% Complete", end="\r")
+
+def process_target(target, semaphore, total_ips, progress_dict):
+    for ip in ipaddress.IPv4Network(target, strict=False):
+        ip_str = ip.compressed
+        semaphore.acquire()  # Acquire a semaphore slot
+        if args.nothreads:
+            try:
+                common_scan(ip_str)
+            finally:
+                with progress_dict['lock']:
+                    progress_dict['progress'] += 1
+                    update_progress_bar(progress_dict['progress'], total_ips, target)
+                semaphore.release()
+        else:
+            thread = threading.Thread(target=lambda: common_scan_thread(ip_str, semaphore, target, progress_dict, total_ips))
+            thread.start()
+
+def common_scan_thread(ip, semaphore, target, progress_dict, total_ips):
+    try:
+        common_scan(ip)
+    finally:
+        with progress_dict['lock']:
+            progress_dict['progress'] += 1
+            update_progress_bar(progress_dict['progress'], total_ips, target)
+        semaphore.release()
 
 def main():
     init()
@@ -86,19 +133,45 @@ def main():
     if not args.nologo:
         banner()
 
-    threads = []
-    for ip in ipaddress.IPv4Network(args.target):
-        if args.nothreads:
-            common_scan(ip.compressed)
-        else:
-            thread = threading.Thread(target=common_scan, args=(ip.compressed,))
+    max_threads = int(args.threads)
+    semaphore = threading.Semaphore(max_threads)
 
-            thread.start()
-            threads.append(thread)
+    target = args.target
 
-    for thread in threads:
-        thread.join()
+    print(f"Scanning: {Fore.GREEN}{target}{Fore.RESET} with {Fore.YELLOW}{max_threads}{Fore.RESET} threads")
+    start_time = datetime.now()
+    print(f"Start scanning {target} at {start_time.strftime('%Y-%m-%d %H:%M:%S')}")
 
+    # Check if target is an IP or IP range, otherwise read from file
+    if is_valid_ip(target) or is_valid_ip_range(target):
+        total_ips = len(list(ipaddress.IPv4Network(target, strict=False)))
+        progress_dict = {'progress': 0, 'lock': threading.Lock()}
+        process_target(target, semaphore, total_ips, progress_dict)
+    else:
+        # Read targets from file
+        try:
+            with open(target, 'r') as file:
+                for line in file:
+                    line = line.strip()
+                    if is_valid_ip(line) or is_valid_ip_range(line):
+                        total_ips = len(list(ipaddress.IPv4Network(line, strict=False)))
+                        progress_dict = {'progress': 0, 'lock': threading.Lock()}
+                        process_target(line, semaphore, total_ips, progress_dict)
+        except FileNotFoundError:
+            print(f"Error: File {target} not found.")
+            return
+        except IOError as e:
+            print(f"IOError: {e}")
+            return
+
+    # Wait for all threads to finish
+    main_thread = threading.current_thread()
+    for t in threading.enumerate():
+        if t is not main_thread:
+            t.join()
+
+    print()
+    
     if args.verbose:
         print(f"Current online: {Fore.GREEN}{len(machines)}{Fore.RESET}")
 
@@ -121,6 +194,15 @@ def main():
             log.error("Error writing to file: not enough permissions.")
         except Exception as e:
             log.error(e)
+
+    end_time = datetime.now()
+    print(f"Finished scanning {target} at {end_time.strftime('%Y-%m-%d %H:%M:%S')}")
+
+    # Calculate duration
+    duration_seconds = (end_time - start_time).total_seconds()
+    duration_minutes = int(duration_seconds // 60)
+    duration_seconds = int(duration_seconds % 60)
+    print(f"Duration for {target}: {Fore.YELLOW}{duration_minutes} minutes {duration_seconds} seconds{Fore.RESET}")
 
 
 if __name__ == "__main__":
